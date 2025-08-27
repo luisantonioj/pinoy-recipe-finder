9472ae255d205238f4f3de85b4d9c4f998ca5ed8
luisantonioj <luisantoniojopia@gmail.com>
2025-08-28 07:00:10 +0800

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import RecipeDetailPage from "./pages/RecipeDetailPage";
+import Home from "./pages/Home";
+import RecipeDetail from "./pages/RecipeDetail";
+import Favorites from "./pages/Favorites";
+import Header from "./components/Header";
 
-function App() {
+export default function App(){
   return (
-    <div className="p-6">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipeDetailPage />} />
-      </Routes>
+    <div className="min-h-screen">
+      <Header />
+      <main className="max-w-6xl mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </div>
   );
 }
-
-export default App;
